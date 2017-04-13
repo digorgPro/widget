4c2cdb48b03d117f24ea4a8f9db1140f6294f629
xh.shao <xh.shao@unicall.com>
2017-04-13 22:35:44 +0800

    0.0.2
    新增图片查看器

--- js/shao-widget.js
+++ js/shao-widget.js
@@ -159,6 +159,95 @@
 	    
 	}
 	
+	//图片查看器
+	$.fn.shaoPic = function(options) {
+		var _this = this,
+			index = 0;
+		defaultVal = {
+			speed: 800
+		};
+		var opt = $.extend(defaultVal, options);
+		var plImgshow = _this.find(".piclist .picimg");
+		var len = plImgshow.length;
+		var showimg = _this.find(".picshow");
+		var imghtml = _this.find(".piclist").html();
+		var plImg;
+		plImgshow.click(function() {
+			$("html,body").css("overflow", "hidden");
+			index = $(this).index();
+			showimg.prepend(imghtml);
+			plImg = showimg.find(".picimg");
+			plImg.eq(index).css("opacity", "1");
+			showimg.animate({
+				top: "0"
+			});
+
+		});
+		var plNext = showimg.find(".next");
+		var plClose = showimg.find(".close");
+
+		if(len > 1) {
+
+			plNext.click(function() {
+				if(!plImg.is(":animated")) { //判断元素是否正处于动画状态
+					plImg.eq(index).css("z-index", "200").animate({
+						top: "100%"
+					}, opt.speed, function() {
+						$(this).css({
+							"top": "0",
+							"opacity": "0",
+							"z-index": "100"
+						});
+
+					});
+					if(index == plImg.length - 1) {
+						plImg.eq(0).css({
+							"opacity": "1",
+							"z-index": "80"
+						});
+						index = 0;
+					} else {
+						plImg.eq((index + 1)).css({
+							"opacity": "1",
+							"z-index": "80"
+						});
+						index = index + 1;
+					}
+				}
+
+			});
+			var browser = window.navigator.userAgent.toLowerCase().indexOf('firefox');
+
+			if(browser != -1) {
+				//处理火狐滚轮事件
+				showimg[0].addEventListener('DOMMouseScroll', function(ev) {
+					var oEvent = ev || event;
+					if(oEvent.detail > 0) { //向上滚动
+						plNext.click();
+					}
+				});
+			} else {
+				//其他浏览器
+				showimg[0].onmousewheel = function(ev) {
+					var oEvent = ev || event; //上下滚轮动作判断
+					if(oEvent.wheelDelta < 0) { //向下滚动
+						plNext.click();
+					}
+				}
+			}
+		}
+		plClose.click(function() {
+
+			showimg.css({
+				"top": "-150%"
+			});
+			plImg.remove();
+			$("html,body").css("overflow", "visible");
+		});
+
+	}
+	
+	
 	//banner轮播
 	
 })(jQuery);
